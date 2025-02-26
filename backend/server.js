86669ec966cf6c273514adc2df9e22cd0e9a7a59
sibilr1016 <sibilr1016@gmail.com>
2025-02-26 15:51:13 +0530

    few updations

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+console.log("NODE_ENV:", process.env.NODE_ENV);
 
 // Body parser middleware
 app.use(express.json());
@@ -21,11 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser middleware
 app.use(cookieParser());
-
+/*
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-
+*/
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -38,6 +39,16 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  console.log("success");
+  // set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+}
 app.use(notFound);
 app.use(errorHandler);
 
